af53a8e39f19bb2606b6958804c96d8361586863
yashi12 <yashiagarwal1812@gmail.com>
2020-09-20 18:18:45 +0530

    Add model: Product class

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
-const products =[];
+const Product = require('../models/product');
 
 const getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
@@ -22,7 +23,9 @@ const getAddProducts = (req, res, next) => {
 };
 
 const postAddProducts =  (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product( req.body.title);
+    product.save();
+    // products.push({ title: req.body.title });
     res.redirect('/');
 };
 
---
+++ models/product.js
@@ -0,0 +1,18 @@
+const products =[];
+
+class Product {
+
+    constructor(title) {
+        this.title = title;
+    }
+
+    save(){
+        products.push(this);
+    }
+
+    static fetchAll(){
+        return products;
+    }
+}
+
+module.exports = Product;
