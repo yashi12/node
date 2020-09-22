489fbe3d76274cbca4d756385bd297ec93ece56a
yashi12 <yashiagarwal1812@gmail.com>
2020-09-22 20:14:35 +0530

    Add links to navbar

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 const getProducts = (req, res, next) => {
      Product.fetchAll((products)=>{
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
@@ -14,7 +14,7 @@ const getProducts = (req, res, next) => {
 };
 
 const getAddProducts = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
