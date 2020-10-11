7746547bbd512290203426030f6c4b56fe474056
yashi12 <yashiagarwal1812@gmail.com>
2020-10-11 22:26:20 +0530

    Connect to database using sequelize
    - define product table structure

--- util/database.js
+++ util/database.js
@@ -1,12 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+//
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-complete',
+//     password:'abcd1234'
+// });
+//
+// module.exports = pool.promise(
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'abcd1234'
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize('node-complete','root','abcd1234',{
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
-
-
+module.exports = sequelize;
