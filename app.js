02022d68fa5802256e0ac9965565e7cf3b6a34f2
yashi12 <yashiagarwal1812@gmail.com>
2020-10-16 02:04:17 +0530

    Sync (user,product) model
    - use force to drop existing tables and make new with relations

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,7 +25,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+User.hasMany(Product);
+
+sequelize.sync({force:true})
     .then(res=>{
         app.listen(3000);
     })
